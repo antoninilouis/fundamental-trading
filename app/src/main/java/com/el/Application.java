5db1fc1a4dde64fbc44fc224898bfc9522250211
louisantonini <louis.antonini@hec.edu>
2022-05-08 20:35:32 +0200

    Convert price rawdata maps' keys to LocalDate

--- app/src/main/java/com/el/Application.java
+++ app/src/main/java/com/el/Application.java
@@ -1,6 +1,7 @@
 package com.el;
 
 import java.io.*;
+import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.LinkedHashMap;
 import java.util.List;
@@ -24,31 +25,34 @@ public class Application
         return 0.0;
     }
 
-    public static Map<String, Double> extractSPReturns() {
+    public static Map<LocalDate, Double> extractSPReturns() {
         // Get prices
-        return byBufferedReader(
-            "SP500 prices (daily)",
-            DupKeyOption.OVERWRITE
+        final Map<LocalDate, Double> prices = byBufferedReader(
+                "SP500 prices (daily)",
+                DupKeyOption.OVERWRITE
         );
-        // todo: Get returns
+        toReturnPercents(prices);
+        return prices;
     }
 
-    public static Map<String, Double> extractStockReturns() {
+    public static Map<LocalDate, Double> extractStockReturns() {
         // Get prices
-        return byBufferedReader(
+        final Map<LocalDate, Double> prices = byBufferedReader(
             "BRK-B prices (daily)",
             DupKeyOption.OVERWRITE
         );
+        toReturnPercents(prices);
+        return prices;
     }
 
-    public static Map<String, Double> extractTBillsReturns() {
+    public static Map<LocalDate, Double> extractTBillsReturns() {
         return byBufferedReader(
-                "t-bills returns (daily)",
-                DupKeyOption.OVERWRITE
+            "t-bills returns (daily)",
+            DupKeyOption.OVERWRITE
         );
     }
 
-    public static void toReturnPercents(final Map<String, Double> prices) {
+    public static void toReturnPercents(final Map<LocalDate, Double> prices) {
         final var iterator = prices.entrySet().iterator();
         if (!iterator.hasNext()) {
             return;
@@ -67,6 +71,8 @@ public class Application
     public static void calculateStockBeta() {
         // todo: do linear regression on stock/s&p returns
         //  /!\ caveats only include days which have returns in both SP500 & stock in the regression
+
+        // build a map: date -> returns1_at_date, returns2_at_date
     }
 
     public static void calculateExpectedReturnsOnMarket() {
@@ -88,24 +94,23 @@ public class Application
         return list;
     }
 
-    public static Map<String, Double> byBufferedReader(String filePath, DupKeyOption dupKeyOption) {
-        LinkedHashMap<String, Double> map = new LinkedHashMap<>();
+    public static Map<LocalDate, Double> byBufferedReader(String filePath, DupKeyOption dupKeyOption) {
+        LinkedHashMap<LocalDate, Double> map = new LinkedHashMap<>();
         String line;
         final var inputStreamReader = new InputStreamReader(getFileFromResourceAsStream(filePath));
         try (BufferedReader reader = new BufferedReader(inputStreamReader)) {
             while ((line = reader.readLine()) != null) {
                 String[] keyValuePair = line.split("\t", 2);
                 if (keyValuePair.length > 1) {
-                    String key = keyValuePair[0];
-                    String value = keyValuePair[1];
-                    var converted = Double.valueOf(value);
-                    if (converted.isNaN()) {
+                    var key = LocalDate.parse(keyValuePair[0]);
+                    var value = Double.valueOf(keyValuePair[1]);
+                    if (value.isNaN()) {
                         continue;
                     }
                     if (DupKeyOption.OVERWRITE == dupKeyOption) {
-                        map.put(key, converted);
+                        map.put(key, value);
                     } else if (DupKeyOption.DISCARD == dupKeyOption) {
-                        map.putIfAbsent(key, converted);
+                        map.putIfAbsent(key, value);
                     }
                 } else {
                     System.out.println("No Key:Value found in line, ignoring: " + line);
--- app/src/test/java/com/el/ApplicationTest.java
+++ app/src/test/java/com/el/ApplicationTest.java
@@ -2,6 +2,7 @@ package com.el;
 
 import org.junit.Test;
 
+import java.time.LocalDate;
 import java.util.LinkedHashMap;
 
 import static org.junit.Assert.assertEquals;
@@ -27,29 +28,29 @@ public class ApplicationTest
     @Test
     public void testConversionToReturnPercents()
     {
-        final var prices = new LinkedHashMap<String, Double>();
-        prices.put("2012-01-03", 77.680000);
-        prices.put("2012-01-04", 76.800003);
-        prices.put("2012-01-05", 76.930000);
-        prices.put("2012-01-06", 76.389999);
-        prices.put("2012-01-09", 76.290001);
-        prices.put("2012-01-10", 77.419998);
-        prices.put("2012-01-11", 78.000000);
-        prices.put("2012-01-12", 78.500000);
-        prices.put("2012-01-13", 77.769997);
-        prices.put("2012-01-17", 77.970001);
+        final var prices = new LinkedHashMap<LocalDate, Double>();
+        prices.put(LocalDate.parse("2012-01-03"), 77.680000);
+        prices.put(LocalDate.parse("2012-01-04"), 76.800003);
+        prices.put(LocalDate.parse("2012-01-05"), 76.930000);
+        prices.put(LocalDate.parse("2012-01-06"), 76.389999);
+        prices.put(LocalDate.parse("2012-01-09"), 76.290001);
+        prices.put(LocalDate.parse("2012-01-10"), 77.419998);
+        prices.put(LocalDate.parse("2012-01-11"), 78.000000);
+        prices.put(LocalDate.parse("2012-01-12"), 78.500000);
+        prices.put(LocalDate.parse("2012-01-13"), 77.769997);
+        prices.put(LocalDate.parse("2012-01-17"), 77.970001);
 
-        final var returns = new LinkedHashMap<String, Double>();
-        returns.put("2012-01-03", 0.0);
-        returns.put("2012-01-04", -0.011328488671472736);
-        returns.put("2012-01-05", 0.0016926692047134484);
-        returns.put("2012-01-06", -0.007019381255687018);
-        returns.put("2012-01-09", -0.0013090457037445713);
-        returns.put("2012-01-10", 0.014811862435288203);
-        returns.put("2012-01-11", 0.007491630263281479);
-        returns.put("2012-01-12", 0.0064102564102563875);
-        returns.put("2012-01-13", -0.009299401273885288);
-        returns.put("2012-01-17", 0.002571737272922814);
+        final var returns = new LinkedHashMap<LocalDate, Double>();
+        returns.put(LocalDate.parse("2012-01-03"), 0.0);
+        returns.put(LocalDate.parse("2012-01-04"), -0.011328488671472736);
+        returns.put(LocalDate.parse("2012-01-05"), 0.0016926692047134484);
+        returns.put(LocalDate.parse("2012-01-06"), -0.007019381255687018);
+        returns.put(LocalDate.parse("2012-01-09"), -0.0013090457037445713);
+        returns.put(LocalDate.parse("2012-01-10"), 0.014811862435288203);
+        returns.put(LocalDate.parse("2012-01-11"), 0.007491630263281479);
+        returns.put(LocalDate.parse("2012-01-12"), 0.0064102564102563875);
+        returns.put(LocalDate.parse("2012-01-13"), -0.009299401273885288);
+        returns.put(LocalDate.parse("2012-01-17"), 0.002571737272922814);
 
         Application.toReturnPercents(prices);
         assertEquals(prices, returns);
